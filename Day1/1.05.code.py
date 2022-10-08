9891cdec38ade6d8a997888dd0060cd560155439
axaish <112846324+axaish@users.noreply.github.com>
2022-10-08 10:11:58 +0530

    Delete Day1 directory

--- Day1/1.05.code.py
+++
@@ -1,8 +0,0 @@
-new_count=int(input("Enter the number of fresh loaves purchased:"))
-old_count=int(input("Enter the number of day old loaves purchased:"))
-rate_old=(185-(0.6*185))*old_count
-rate_new=185*new_count
-print("Regular Price: Rs.185.00")
-print("Amount of new loaves: Rs.",rate_new)
-print("Amount of old loaves: Rs.",rate_old)
-print("Total Amount: Rs.",rate_old+rate_new)
